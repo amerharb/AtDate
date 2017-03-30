a4bba53da3cfb58bb7fd4657e717e858bf8edd5b
Amer Harb <amer.harb@gmail.com>
2017-03-30 09:05:42 +0200

    simplify condition

--- src/AtDateConverter.java
+++ src/AtDateConverter.java
@@ -5,7 +5,7 @@
  */
 public class AtDateConverter {
 
-    public static String getAtDateBits(int year, int month, int day, int hour, int min, int sec) {
+    private static String getAtDateBits(int year, int month, int day, int hour, int min, int sec) {
 
         StringBuilder sb = new StringBuilder();
 
@@ -13,11 +13,11 @@ public class AtDateConverter {
 
         if (year >= 0 && year < 10) {
             sb.append("000" + year);
-        } else if (year >= 10 && year < 100) {
+        } else if (year < 100) {
             sb.append("00" + year);
-        } else if (year >= 100 && year < 1000) {
+        } else if (year < 1000) {
             sb.append("0" + year);
-        } else if (year >= 1000 && year < 10000) {
+        } else if (year < 10000) {
             sb.append(year);
         } else {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -42,22 +42,22 @@ public class AtDateConverter {
         return getAtDateBits(sb.toString());
     }
 
-    public static String getAtDateBits(int year, int month, int day, int hour, int min) {
+    private static String getAtDateBits(int year, int month, int day, int hour, int min) {
 
         return getAtDateBits(year, month, day, hour, min, 0);
     }
 
-    public static String getAtDateBits(int year, int month, int day, int hour) {
+    private static String getAtDateBits(int year, int month, int day, int hour) {
 
         return getAtDateBits(year, month, day, hour, 0, 0);
     }
 
-    public static String getAtDateBits(int year, int month, int day) {
+    private static String getAtDateBits(int year, int month, int day) {
 
         return getAtDateBits(year, month, day, 0, 0, 0);
     }
 
-    public static String getAtDateBits(int year, int month) {
+    private static String getAtDateBits(int year, int month) {
 
         return getAtDateBits(year, month, 1, 0, 0, 0);
     }
