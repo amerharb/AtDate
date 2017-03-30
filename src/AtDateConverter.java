a9e2cc76ea5b4a61fbe3c1eafbdf346bdeeb86c5
Amer Harb <amer.harb@gmail.com>
2017-03-30 09:29:32 +0200

    write code for months and days, still leap year not supported

--- src/AtDateConverter.java
+++ src/AtDateConverter.java
@@ -11,7 +11,17 @@ public class AtDateConverter {
 
         sb.append("@");
 
-        if (year >= 0 && year < 10) {
+        if (year <= -10000) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        } else if (year < -999) {
+            sb.append("-" + Math.abs(year));
+        } else if (year < -99) {
+            sb.append("-0" + Math.abs(year));
+        } else if (year < -9) {
+            sb.append("-00" + Math.abs(year));
+        } else if (year < 0) {
+            sb.append("-000" + Math.abs(year));
+        } else if (year < 10) {
             sb.append("000" + year);
         } else if (year < 100) {
             sb.append("00" + year);
@@ -27,6 +37,32 @@ public class AtDateConverter {
             sb.append("0" + min);
         } else if (min >= 10 && min < 60) {
             sb.append(min);
+        } else {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        if (month >= 1 && month < 10) {
+            sb.append("0" + month);
+        } else if (month >= 10 && month <= 12) {
+            sb.append(month);
+        } else {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        int maxDay;
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            maxDay = 31;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            maxDay = 30;
+        } else {
+            maxDay = 28;
+            //TODO: leap year
+        }
+
+        if (day >= 1 && day < 10) {
+            sb.append("0" + day);
+        } else if (month >= 10 && month <= 12) {
+            sb.append(month);
         } else {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
